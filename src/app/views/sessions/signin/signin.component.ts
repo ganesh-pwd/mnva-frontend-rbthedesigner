18bd34c6b2bef4a1456ca2d94576843ffb99b399
Philippe <philippe@mnrva.io>
2019-03-18 14:42:16 -0600

    Arreglando los 2 files de Login que habíamos modificado los 2

--- src/app/views/sessions/signin/signin.component.ts
+++ src/app/views/sessions/signin/signin.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { MatButton } from '@angular/material';
+import { MatProgressBar, MatButton } from '@angular/material';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { map } from 'rxjs/operators';
@@ -12,6 +12,7 @@ import { egretAnimations } from '../../../shared/animations/egret-animations';
   styleUrls: ['./signin.component.scss']
 })
 export class SigninComponent implements OnInit {
+  @ViewChild(MatProgressBar) progressBar: MatProgressBar;
   @ViewChild(MatButton) submitButton: MatButton;
 
   signinForm: FormGroup;
@@ -22,23 +23,13 @@ export class SigninComponent implements OnInit {
     this.signinForm = new FormGroup({
       username: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required),
-<<<<<<< HEAD
       rememberMe: new FormControl(false)
-=======
-      rememberMe: new FormControl(true)
->>>>>>> 5b23fc1deadab64d1841200b143b364d0d0f1ac5
     })
   }
 
   signin() {
     const signinData = this.signinForm.value
     console.log(signinData);
-<<<<<<< HEAD
-=======
-
-    this.submitButton.disabled = true;
-  }
->>>>>>> 5b23fc1deadab64d1841200b143b364d0d0f1ac5
 
     this.submitButton.disabled = true;
     this.progressBar.mode = 'indeterminate';
