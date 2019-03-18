2b82f138206615d6b9692063cec3fe6c9d43fb18
Stephane <s@mnrva.io>
2019-03-18 17:39:09 -0600

    Corrigiendo Errores

--- src/app/views/products/algorithm-credit/algorithm-credit.datasource.ts
+++ src/app/views/products/algorithm-credit/algorithm-credit.datasource.ts
@@ -15,31 +15,26 @@ const EXAMPLE_DATA: DataTableItem[] = [
   {
       'id': 0,
       'algorithm': 'Sentiment',
-      'description': ' Vim falli insolens persequeris no. Eu pri aeque semper, dicunt malorum reprehendunt ex mea, viderer inimicus vim ne.',
       'price': '$0.00004 mention'
     },
     {
       id: 1,
       'algorithm': 'Gender author',
-      'description': ' Vim falli insolens persequeris no. Eu pri aeque semper, dicunt malorum reprehendunt ex mea, viderer inimicus vim ne.',
       'price': '$0.00004 mention'
     },
     {
       'id': 2,
       'algorithm': 'Image recognition',
-      'description': ' Vim falli insolens persequeris no. Eu pri aeque semper, dicunt malorum reprehendunt ex mea, viderer inimicus vim ne.',
       'price': '$0.00004 mention'
     },
     {
       'id': 3,
       'algorithm': 'Lorem Ipsum',
-      'description': ' Vim falli insolens persequeris no. Eu pri aeque semper, dicunt malorum reprehendunt ex mea, viderer inimicus vim ne.',
       'price': '$0.00004 mention'
     },
     {
       'id': 4,
       'algorithm': 'Ipsum',
-      'description': ' Vim falli insolens persequeris no. Eu pri aeque semper, dicunt malorum reprehendunt ex mea, viderer inimicus vim ne.',
       'price': '$0.00004 mention'
     },
 ];
