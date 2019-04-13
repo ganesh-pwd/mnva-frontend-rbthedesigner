729754923f875f7a1ba9bc313bc755afda75cf68
Stephane <s@mnrva.io>
2019-04-12 18:23:21 -0600

    Merge de algunos cambios. Estilos y revisión al código de Luis.

--- src/app/shared/fake-db/datasource.ts
+++ src/app/shared/fake-db/datasource.ts
@@ -5,10 +5,7 @@ export class DatasourceDB {
     { name: 'Web'},
     { name: 'AirBnB' },
     { name: 'Waze' },
-    // { name: 'All Facebook Pages'},
-    // { name: 'Owned Facebook Profile'},
-    // { name: 'All Twitter Profiles'},
-    // { name: 'Owned Twitter Profile'},
-    // { name: 'Media Websites'}
+    { name: 'Owned Facebook Page'},
+    { name: 'Owned Twitter Profile'},
   ];
 }
